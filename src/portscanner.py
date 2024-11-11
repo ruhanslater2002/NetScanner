7bdf9f085b0118d3efac9b6a231db1f6355b5325
Ruhan <ruhan.slater@gmail.com>
2024-11-11 12:53:17 +0200

    Changed port scanner logger

--- src/portscanner.py
+++ src/portscanner.py
@@ -10,7 +10,8 @@ class PortScanner:
         self.logger = ConsoleLogger("PORT SCANNER")
 
     def scan_ports(self, target_port: int, port_range: int) -> None:
-        self.logger.info(f"[*] Scanning ports on {colored(self.target_ip, 'green')}\n")
+        self.logger.info(f"Scanning ports on {colored(self.target_ip, 'green')}\n")
+        open_ports_found: int = 0
         if port_range < target_port:
             port_range = target_port + port_range + 1
         for port in range(target_port, port_range):
@@ -20,4 +21,7 @@ class PortScanner:
             # Checks if response is a SYN-ACK, meaning the port is open
             if response:
                 port_services: str = PortServices(port).get_port_service()
-                self.logger.info(f' | {port:<7} | {colored("OPEN", "green")} \t| {port_services}')
+                self.logger.info(f'PORT {colored(port, "green")} ({port_services}) is {colored("OPEN", "green")}')
+                open_ports_found += 1
+        print('')
+        self.logger.info(f'Found {colored(open_ports_found, "green")} ports open.\n')
