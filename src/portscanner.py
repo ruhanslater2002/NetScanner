dffb20407a51c3bd525b431b95ca6ce4bf090edd
Ruhan <ruhan.slater@gmail.com>
2024-11-11 12:58:14 +0200

    changed logger outputs

--- src/portscanner.py
+++ src/portscanner.py
@@ -5,19 +5,18 @@ from consolelogger import ConsoleLogger
 
 
 class PortScanner:
-    def __init__(self, target_ip):
+    def __init__(self, target_ip: str):
         self.target_ip = target_ip
         self.logger = ConsoleLogger("PORT SCANNER")
 
     def scan_ports(self, target_port: int, port_range: int) -> None:
-        self.logger.warning(f"Scanning ports on {colored(self.target_ip, 'green')}\n")
+        self.logger.warning(f"Scanning ports on {colored(self.target_ip, 'green')} ...\n")
         open_ports_found: int = 0
         if port_range < target_port:
             port_range = target_port + port_range + 1
         for port in range(target_port, port_range):
             packet_handler: PacketHandler = PacketHandler(self.target_ip)
             response: bool = packet_handler.send_tcp_packet(port)
-
             # Checks if response is a SYN-ACK, meaning the port is open
             if response:
                 port_services: str = PortServices(port).get_port_service()
