12e65d4eb26b67eeaa7c3e82015c746370fb0350
Ruhan <ruhan.slater@gmail.com>
2024-11-11 13:15:17 +0200

    changed output format for port scanner

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@ from scanner import Scanner
 class Main:
     def __init__(self):
         # self.target_ip = "192.168.0.0/24"
-        self.target_ip = "192.168.0.1"
+        self.target_ip = "google.com"
         # self.target_ip = "192.168.0.47"
         self.scanner: Scanner = Scanner(self.target_ip)
 
     def start(self) -> None:
         # self.scanner.scan_network()
-        self.scanner.scan_ports(1, 80)
+        self.scanner.scan_ports(1, 500)
 
 
 if __name__ == '__main__':
